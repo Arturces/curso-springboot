1f924d867ff2639340281c8c3e3ba861871eceb4
Artur César <arturcesar57@gmail.com>
2022-10-11 08:59:44 -0300

    Inserção de Usuario no banco de dados com o PostMapping no UserResource

--- src/main/java/com/example/cursospringboot/resources/UserResource.java
+++ src/main/java/com/example/cursospringboot/resources/UserResource.java
@@ -4,11 +4,10 @@ import com.example.cursospringboot.entities.User;
 import com.example.cursospringboot.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController //Annotation controlador especial usado para serviços RESTFul
@@ -31,10 +30,17 @@ public class UserResource {
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping(value = "/{id}") //exemplo de endPoints
+    @GetMapping(value = "/{id}") //exemplo de endPoints par abuscar pelo ID do usuario, {1} /user/1 ele faz a busca pelo ID
     public ResponseEntity<User> findById(@PathVariable Long id){
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping //Inserindo um recurso no banco de dados
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 
