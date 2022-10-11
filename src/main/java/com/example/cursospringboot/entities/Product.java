233cf109fcfefc39f498cb499463b1ac9a9112dc
Artur César <arturcesar57@gmail.com>
2022-10-11 07:55:25 -0300

    Associação muitos para muitos entre Product e OrderItem

--- src/main/java/com/example/cursospringboot/entities/Product.java
+++ src/main/java/com/example/cursospringboot/entities/Product.java
@@ -1,5 +1,7 @@
 package com.example.cursospringboot.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
@@ -22,6 +24,9 @@ public class Product implements Serializable {
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();//coleção de categorias
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();//coleção de itens na classe product, fazendo relação com orderitem
 
     public Product() {
     }
@@ -78,6 +83,15 @@ public class Product implements Serializable {
         return categories;
     }
 
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -90,6 +104,5 @@ public class Product implements Serializable {
     public int hashCode() {
         return Objects.hash(id);
     }
-
 
 }
