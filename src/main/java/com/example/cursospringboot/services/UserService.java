7fe9d1a975bd77ba14c8055514a92a18886591b1
Artur César <arturcesar57@gmail.com>
2022-10-11 11:10:37 -0300

    Tratamento de execeção no Update de usuario

--- src/main/java/com/example/cursospringboot/services/UserService.java
+++ src/main/java/com/example/cursospringboot/services/UserService.java
@@ -9,43 +9,48 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 
 //@Component //registra a sua classe como um componente do Spring
 @Service
-public class  UserService {
+public class UserService {
 
     @Autowired//infejeção de dependencia do Service para a classe Controller
     private UserRepository repository;
 
-    public List<User> findAll(){ //buscar todos os users, exemplo de endPoints
+    public List<User> findAll() { //buscar todos os users, exemplo de endPoints
         return repository.findAll();
     }
 
-    public User findById(Long id){
-       Optional<User> obj = repository.findById(id); //Retorna um Optional com o valor fornecido, ja retornar o OBJ do banco
-       return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id); //Retorna um Optional com o valor fornecido, ja retornar o OBJ do banco
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
-    public User insert(User obj){
-       return repository.save(obj);
+    public User insert(User obj) {
+        return repository.save(obj);
     }
 
     public void delete(Long id) {
         try {
             repository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
-            throw  new ResourceNotFoundException(id);//ver o tipo de excecao lançada
+            throw new ResourceNotFoundException(id);//ver o tipo de excecao lançada
         } catch (DataIntegrityViolationException e) {
             throw new DatabaseException(e.getMessage());
         }
     }
 
-    public User update(Long id, User obj){
-        User entity = repository.getOne(id);//prepara o objeto monitorado, somente pra vc mexer e depois alterar no banco
-        updateData(entity,obj);
-        return repository.save(entity);
+    public User update(Long id, User obj) {
+        try {
+            User entity = repository.getOne(id);//prepara o objeto monitorado, somente pra vc mexer e depois alterar no banco
+            updateData(entity, obj);
+            return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
